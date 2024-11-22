9c5c7f0fe3b4d60a884a701ff627f541e46ec1df
FranciscoAnjos <fcariaanjos@gmail.com>
2024-11-22 10:42:24 +0000

    Ask for input and print result

--- calc_stack.py
+++ calc_stack.py
@@ -1,1 +1,11 @@
 ﻿from collections import deque
+
+input_numbers = input()
+numbers = list(map(int, input_numbers.split()))
+
+stack = deque(numbers)
+
+print(f"deque({list(stack)})")
+
+while stack:
+    print(stack.pop() ** 2)
